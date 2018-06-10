dfec1e15f6c3a6e3f403415cc32f07fc731f1b4d
Ivan <wangfusion@gmail.com>
2018-06-10 16:37:00 +0800

    mod5
    mod5

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,6 +9,11 @@ class Quiz extends Component {
     super(props)
     this.state = {quiz_position: 1}
   }
+  showNextQuestion() {
+    this.setState((state) => {
+      return { quiz_position: state.quiz_position + 1 }
+    })
+  }
   render() {
     const isQuizEnd = (this.state.quiz_position - 1) == quizData.quiz_questions.length
     if (isQuizEnd)
@@ -18,7 +23,8 @@ class Quiz extends Component {
     else {
       return (
         <div>
-          <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+          <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+                        showNextQuestionHandler={this.showNextQuestion.bind(this)} />
         </div>
       )
     }
